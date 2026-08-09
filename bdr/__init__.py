6b9c9f4746a415bf539b3550fc79e4ec001bd97f
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-09 12:49:21 -0300

    feat: initialize BDR package

---
+++ bdr/__init__.py
@@ -0,0 +1,9 @@
+"""Resolutive Database Engine (BDR) public API."""
+
+from .core import BancoDeDadosResolutivo, EncoderResolutivo, EntidadeResolutiva
+
+__all__ = [
+    "BancoDeDadosResolutivo",
+    "EncoderResolutivo",
+    "EntidadeResolutiva",
+]
