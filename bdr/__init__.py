18e49053354fda06691910f64397fb206ea623e3
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-09 14:56:44 -0300

    api: expose persistent engine in v0.1.0

--- bdr/__init__.py
+++ bdr/__init__.py
@@ -1,9 +1,15 @@
 """Resolutive Database Engine (BDR) public API."""
 
 from .core import BancoDeDadosResolutivo, EncoderResolutivo, EntidadeResolutiva
+from .persistent_engine import PersistentBDR, RecoveryError
+
+__version__ = "0.1.0"
 
 __all__ = [
     "BancoDeDadosResolutivo",
     "EncoderResolutivo",
     "EntidadeResolutiva",
+    "PersistentBDR",
+    "RecoveryError",
+    "__version__",
 ]
