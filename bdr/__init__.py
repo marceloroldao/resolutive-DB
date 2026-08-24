85b46f39dc2c254f1f4b785fafa560b15b50bacd
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-24 01:15:45 -0300

    fix(release): align legacy Python version with v0.2.0-rc1

--- bdr/__init__.py
+++ bdr/__init__.py
@@ -3,7 +3,7 @@
 from .core import BancoDeDadosResolutivo, EncoderResolutivo, EntidadeResolutiva
 from .persistent_engine import PersistentBDR, RecoveryError
 
-__version__ = "0.1.0"
+__version__ = "0.2.0rc1"
 
 __all__ = [
     "BancoDeDadosResolutivo",
