87a732bf99ee2f3c2be5faff5d672da0830f2f56
Marcelo Roldão Matos <marceloroldao@gmail.com>
2026-08-27 20:55:55 -0300

    explorer: export observation and event primitives

--- explorer/__init__.py
+++ explorer/__init__.py
@@ -1,5 +1,13 @@
 """Read-only visual explorer for the Resolutive Database Engine."""
 
 from .adapter import ExplorerSnapshotProvider
+from .events import EventBuffer, EventKind, ExplorerEvent
+from .observation import PublicBDRObservationProvider
 
-__all__ = ["ExplorerSnapshotProvider"]
+__all__ = [
+    "EventBuffer",
+    "EventKind",
+    "ExplorerEvent",
+    "ExplorerSnapshotProvider",
+    "PublicBDRObservationProvider",
+]
